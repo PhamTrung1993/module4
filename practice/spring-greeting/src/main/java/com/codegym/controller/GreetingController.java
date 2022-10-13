20aa74c562b25a026a0347871f4f12c94222137b
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-10-13 21:44:05 +0700

    spring-greeting

---
+++ practice/spring-greeting/src/main/java/com/codegym/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+//@Controller: là annotation để đánh dấu lớp GreetingController là một controller (đối tượng sẽ xử lý các request)
+//@GetMapping: là annotation để đánh dấu phương thức greeting()
+// là một phương thức dùng để xử lý request khi có một request GET được gửi đến đường dẫn "/greeting".
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    //@RequestParam: là annotation để đánh dấu rằng name là tham số được truyền vào
+    //model: là đối tượng để truyền dữ liệu từ controller sang view
+    //model.addAttribute(): là phương thức để truyền một dữ liệu từ controller sang view
+    public String greeting(@RequestParam String name, Model model) {
+        model.addAttribute("name",name);
+        return "index";
+    }
+
+}
---
+++ practice/spring-greeting/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 10/13/2022
+  Time: 9:40 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Greeting</title>
+</head>
+<body>
+<h1>Hello: ${name}</h1>
+</body>
+</html>
