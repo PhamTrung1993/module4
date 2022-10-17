17dbab03de2f368484cc422339809e7a083a5e88
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-10-17 22:38:29 +0700

    fix server

--- practice/customer-management-thymleaf/src/main/java/com/codegym/controller/CustomerController.java
+++ practice/customer-management-thymleaf/src/main/java/com/codegym/controller/CustomerController.java
@@ -6,14 +6,13 @@ import com.codegym.service.CustomerService;
 import com.codegym.service.ICustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
     private final ICustomerService customerService = new CustomerService();
 
@@ -34,10 +33,20 @@ public class CustomerController {
         customerService.save(customer);
         return "redirect:/customer";
     }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findByID(id));
+        return "/edit";
+    }
     @PostMapping("/update")
     public String update(Customer customer) {
         customerService.update(customer.getId(), customer);
         return "redirect:/customer";
+    }
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findByID(id));
+        return "/delete";
     }
     @PostMapping("/delete")
     //RedirectAttributes được dùng để truyền các giá trị, tham số giữa các controller khi thực hiện redirect:
